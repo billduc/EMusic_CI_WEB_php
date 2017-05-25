3a033f540dc02b1d5b8cfebb7837346b4d5c3788
eitguide <eitguide.np@gmail.com>
2017-05-25 16:08:03 +0700

    add BaseModel and BackendBase

--- application/controllers/BackendBase.php
+++ application/controllers/BackendBase.php
@@ -6,7 +6,14 @@
  * Date: 5/25/17
  * Time: 3:42 PM
  */
-class BackendBase
+class BackendBase extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
 
+        if(!$this->session->userdata('is_admin')){
+            redirect(base_url());
+        }
+    }
 }
--- application/models/BaseModel.php
+++ application/models/BaseModel.php
@@ -6,7 +6,36 @@
  * Date: 5/25/17
  * Time: 3:48 PM
  */
-class BaseModel
+class BaseModel extends CI_Model
 {
+    private $table;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function setTable($table)
+    {
+        $this->$table = $table;
+    }
+
+    public function set_data($data, $condition = [])
+    {
+        if(empty($condition)){
+            return $this->db->insert($this->table, $data);
+        }
+
+        $this->db->where($condition);
+        return $this->db->update($this->table, $data);
+
+    }
+
+    public function find_all($condition=[]){
+        $this->db->where($condition);
+        return $this->db->get($this->table)->result_array();
+    }
+
 
 }
