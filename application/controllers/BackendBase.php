ae9d667cdb262b4dbb90a589b83702aee863c82d
eitguide <eitguide.np@gmail.com>
2017-05-25 16:06:55 +0700

    create master layout

---
+++ application/controllers/BackendBase.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: nguyennghia
+ * Date: 5/25/17
+ * Time: 3:42 PM
+ */
+class BackendBase
+{
+
+}
---
+++ application/models/BaseModel.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: nguyennghia
+ * Date: 5/25/17
+ * Time: 3:48 PM
+ */
+class BaseModel
+{
+
+}
